0f9049de968e2dcf672e3a2039c73abed5689067
pc <asembarak1010@gmail.com>
2026-04-13 21:04:58 +0200

    fix C3 error

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,17 +1,17 @@
 package tests;
 
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import pages.LoginPage;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class LoginTest {
 
     @Test
     public void testLogin(){
+
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
@@ -21,13 +21,28 @@ public class LoginTest {
 
         WebDriver driver = new ChromeDriver(options);
 
-        driver.get("http://demo.guru99.com/V4/");
+        try {
+            driver.get("http://demo.guru99.com/V4/");
 
-        LoginPage login = new LoginPage(driver);
-        login.login("mngr559943","AqajysY");
+            LoginPage login = new LoginPage(driver);
+            login.login("mngr559943", "AqajysY");
 
-        Assert.assertTrue(driver.getPageSource().contains("Manger Id"));
 
-        driver.quit();
+            Thread.sleep(3000);
+
+            // ✅ Assertion
+            String pageSource = driver.getPageSource();
+
+            Assert.assertTrue(
+                    pageSource.contains("Manger Id"),
+                    "Login Failed - Text not found"
+            );
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("Test Failed بسبب Exception");
+        } finally {
+            driver.quit();
+        }
     }
 }
