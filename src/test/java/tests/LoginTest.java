bc42e7e1f18418ccd71c2bc24d5f1fcbcf42c81c
pc <asembarak1010@gmail.com>
2026-04-13 21:02:13 +0200

    fix CI2 error

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -6,13 +6,20 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import pages.LoginPage;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class LoginTest {
 
     @Test
     public void testLogin(){
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
 
         driver.get("http://demo.guru99.com/V4/");
 
